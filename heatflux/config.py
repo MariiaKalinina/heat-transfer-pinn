7140b37786e3a5584575b0d7c73318bc349bb495
MariiaKalinina <101808180+MariiaKalinina@users.noreply.github.com>
2025-11-14 17:58:13 +0300

    new changes in def-test

--- heatflux/config.py
+++ heatflux/config.py
@@ -1,11 +1,10 @@
 
 import numpy as np
 
-# Reproducibility
+
 TORCH_SEED = 0
 NP_RNG = np.random.default_rng(42)
 
-# Data params
 WELLS = 5
 N_SAMPLES = 500
 TIME_START, TIME_END = 0.0, 260.0  # Myr
